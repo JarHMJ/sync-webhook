81cb448e51fdb795931115f49290f2dfe97e2251
huangminjie <minjie.huang@daocloud.io>
2021-08-22 01:18:03 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import secrets
 import subprocess
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
 app = FastAPI()
 
@@ -15,15 +17,29 @@ class Config:
 
 config = Config()
 
+security = HTTPBasic()
+
+
+def check_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, config.username)
+    correct_password = secrets.compare_digest(credentials.password, config.password)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+
+basic_auth = Depends(check_basic_auth)
+
 
 def sync_repo(path: str):
     os.chdir(path)
-    result = subprocess.run(["git", "log"])
-    result = subprocess.run(["git", "pull", f"origin {config.branch}:{config.branch} --force"])
-    result.check_returncode()
+    subprocess.run(f"git pull origin {config.branch}:{config.branch} --force -v", shell=True, check=True)
 
 
-@app.post("/sync")
+@app.post("/sync", dependencies=[basic_auth])
 async def sync_view():
     sync_repo(config.repo_path)
     return {"result": "ok"}
